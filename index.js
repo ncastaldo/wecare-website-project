e917d56726e41b35d02b428ca82a0e57e99c84b7
Nicola Castaldo <nicola.castaldo@mail.polimi.it>
2018-05-16 15:19:32 +0200

    minor: test on nodejs

--- index.js
+++ index.js
@@ -1,127 +1,56 @@
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
-const sqlDbFactory = require("knex");
-const process = require("process");
+let express = require("express");
+let app = express();
+let process = require("process");
+let bodyParser = require("body-parser");
+let _ = require("lodash");
+let myPort = process.env.PORT | 3000; // if port is not defined, it is 3000 (heroku VS locally)
 
-let sqlDb;
 
-function initSqlDB() {
-  /* Locally we should launch the app with TEST=true to use SQLlite:
-
-       > TEST=true node ./index.js
-
-    */
-  if (process.env.TEST) {
-    sqlDb = sqlDbFactory({
-      client: "sqlite3",
-      debug: true,
-      connection: {
-        filename: "./petsdb.sqlite"
-      },
-      useNullAsDefault: true
-    });
-  } else {
-    sqlDb = sqlDbFactory({
-      debug: true,
-      client: "pg",
-      connection: process.env.DATABASE_URL,
-      ssl: true
-    });
-  }
-}
-
-function initDb() {
-  return sqlDb.schema.hasTable("pets").then(exists => {
-    if (!exists) {
-      sqlDb.schema
-        .createTable("pets", table => {
-          table.increments();
-          table.string("name");
-          table.integer("born").unsigned();
-          table.enum("tag", ["cat", "dog"]);
-        })
-        .then(() => {
-          return Promise.all(
-            _.map(petsList, p => {
-              delete p.id;
-              return sqlDb("pets").insert(p);
-            })
-          );
-        });
-    } else {
-      return true;
-    }
-  });
-}
-
-const _ = require("lodash");
-
-let serverPort = process.env.PORT || 5000;
-
-let petsList = require("./petstoredata.json");
-
-app.use(express.static(__dirname + "/public"));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
-// /* Register REST entry point */
-app.get("/pets", function(req, res) {
-  let start = parseInt(_.get(req, "query.start", 0));
-  let limit = parseInt(_.get(req, "query.limit", 5));
-  let sortby = _.get(req, "query.sort", "none");
-  let myQuery = sqlDb("pets");
+let peopleData = require("./public/assets/json/people.json");
 
-  if (sortby === "age") {
-    myQuery = myQuery.orderBy("born", "asc");
-  } else if (sortby === "-age") {
-    myQuery = myQuery.orderBy("born", "desc");
-  }
-  myQuery
-    .limit(limit)
-    .offset(start)
-    .then(result => {
-      res.send(JSON.stringify(result));
+app.use(express.static(__dirname + "/public"));
+app.set("port", myPort);
+
+app.get("/people", function(req, res){
+    res.send(JSON.stringify(peopleData));
+});
+
+app.get("/people/:id", function(req,res) {
+    
+    let personId = req.params.id;
+    let found = false;
+    
+    console.log(" --------- "+personId+" ---------- ");
+    
+    let person = _.find(peopleData.people, function(o){
+        return o.id === personId;
     });
+    if(_.isUndefined(person)){
+        res.status(400);
+        res.send({message : "Person not found!"});
+    }else{
+        res.status(200);
+        res.send(JSON.stringify(person));
+    }
+    
+    /*
+    
+    for(let i = 0; i<peopleData.people.length && !found; i++){
+        if(peopleData.people[i].id === personId){
+            res.send(JSON.stringify(peopleData.people[i]));
+            found = true;
+        }
+    }
+    if(!found){
+        res.status(400);
+        res.send({message : "Person not found!"});
+    }*/
 });
 
-app.delete("/pets/:id", function(req, res) {
-  let idn = parseInt(req.params.id);
-  sqlDb("pets")
-    .where("id", idn)
-    .del()
-    .then(() => {
-      res.status(200);
-      res.send({ message: "ok" });
-    });
-});
-
-app.post("/pets", function(req, res) {
-  let toappend = {
-    name: req.body.name,
-    tag: req.body.tag,
-    born: req.body.born
-  };
-  sqlDb("pets")
-    .insert(toappend)
-    .then(ids => {
-      let id = ids[0];
-      res.send(_.merge({ id, toappend }));
-    });
-});
-
-// app.use(function(req, res) {
-//   res.status(400);
-//   res.send({ error: "400", title: "404: File Not Found" });
-// });
-
-app.set("port", serverPort);
-
-initSqlDB();
-initDb();
-
-/* Start the server on port 3000 */
-app.listen(serverPort, function() {
-  console.log(`Your app is ready at port ${serverPort}`);
-});
+app.listen(myPort, function(){
+    console.log("The server is running on port: " + myPort);
+})
