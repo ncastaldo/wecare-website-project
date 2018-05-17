e062c9d17a6f130b6b964b66955ecb2d95c2f138
Nicola Castaldo <nicola.castaldo@mail.polimi.it>
2018-05-17 19:27:49 +0200

    location(s).js work with rest endpoint, still some problems on photogallery and service-button

--- public/assets/js/location.js
+++ public/assets/js/location.js
@@ -1,62 +1,68 @@
-var serviceId;
+var URL = function () {
+    // This function is anonymous, is executed immediately and 
+    // the return value is assigned to QueryString!
+    var query_string = {};
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i=0;i<vars.length;i++) {
+        var pair = vars[i].split("=");
+        // If first entry with this name
+        if (typeof query_string[pair[0]] === "undefined") {
+            query_string[pair[0]] = decodeURIComponent(pair[1]);
+            // If second entry with this name
+        } else if (typeof query_string[pair[0]] === "string") {
+            var arr = [ query_string[pair[0]],decodeURIComponent(pair[1]) ];
+            query_string[pair[0]] = arr;
+            // If third or later entry with this name
+        } else {
+            query_string[pair[0]].push(decodeURIComponent(pair[1]));
+        }
+    }
+    return query_string;
+}();
 
 $(document).ready(function(){
-    
-    $.getScript("/assets/js/locations.js",function(){
-        getData().done(loadLocationData);
+
+    $.ajax({
+        method: "GET",
+        dataType: "json",
+        url: "/rest/locations/"+URL.id,
+        error: function (request, error) {
+            console.log(request, error);
+        },
+        success: function(json){ loadJsonIntoHtml(json);}
     });
-    
+
 });
 
-//LOAD THE RESPONSE DATA IN HTML
-function loadLocationData(json){
-    
-    for (let j = 0; j < json.locations.length; j++) {
-    
-        if(URL.id === json.locations[j].id){
-            
-            let location = json.locations[j];
-            
-            $(".LOCATION-NAME").text(location.name);
-            $(".LOCATION-DESCRIPTION").html(location.description);
-            $(".LOCATION-IMAGE").html('<img src="'+location.image+'" style="max-width:70%; min-width: 200px;" />');
-            $(".LOCATION-PRACTICAL-INFO").html(location.practicalInfo);
+function loadJsonIntoHtml(currentLocation){
 
-            let el= "";
-            for(let i=0; i<location.photogallery.length; i++){
-                el += '<div class="col-lg-4 col-md-6 col-12 d-block mb-4 h-100"><img class="img-fluid img-thumbnail" src="'+location.photogallery[i].image+'" alt=""></div>'
-            }
-            $(".LOCATION-PHOTOGALLERY").append(el);        
-            
+    $(".LOCATION-NAME").text(currentLocation.name);
+    $(".LOCATION-DESCRIPTION").html(currentLocation.description);
+    $(".LOCATION-IMAGE").html('<img class="animated fadeInRight" src="'+currentLocation.image+'" style="max-width:70%; min-width: 200px;" />');
+    $(".LOCATION-PRACTICAL-INFO").html(currentLocation.practicalInfo);
+
+    let el= "";
+    
+    $.ajax({
+        url : currentLocation.photogallery,
+        success: function (data) {
+            $(data).find("a").attr("href", function (i, val) {
+                if( val.match(/\.(jpe?g|png|gif)$/) ) { 
+                    el += '<div class="col-lg-4 col-md-6 col-12 d-block mb-4 h-100"><img class="img-fluid img-thumbnail" src="'+currentLocation.photogallery+ val+'" alt=""></div>';
+                } 
+            });
         }
+    });
     
-    }   
+    $(".LOCATION-PHOTOGALLERY").append(el);        
+
+        /*
+    $('.SERVICES-BUTTON').click(function(){
+        console.log("hey");
+        window.location.href = '/pages/services.html?locationId='+currentLocation.id;
+    });
     
+    */
+
 }
-
-
-
-
-
-var URL = function () {
-  // This function is anonymous, is executed immediately and 
-  // the return value is assigned to QueryString!
-  var query_string = {};
-  var query = window.location.search.substring(1);
-  var vars = query.split("&");
-  for (var i=0;i<vars.length;i++) {
-    var pair = vars[i].split("=");
-        // If first entry with this name
-    if (typeof query_string[pair[0]] === "undefined") {
-      query_string[pair[0]] = decodeURIComponent(pair[1]);
-        // If second entry with this name
-    } else if (typeof query_string[pair[0]] === "string") {
-      var arr = [ query_string[pair[0]],decodeURIComponent(pair[1]) ];
-      query_string[pair[0]] = arr;
-        // If third or later entry with this name
-    } else {
-      query_string[pair[0]].push(decodeURIComponent(pair[1]));
-    }
-  }
-  return query_string;
-}();
--- public/assets/js/locations.js
+++ public/assets/js/locations.js
@@ -1,38 +1,29 @@
 $(document).ready(function(){
     
-    getData().done(loadData);
-    
-    
-    
-});
-
-function getData(){
-    
-    return $.ajax({
+    $.ajax({
         method: "GET",
         dataType: "json",
-        url: "/assets/json/locations.json", //SERVER URL
+        url: "/rest/locations",
         error: function (request, error) {
             console.log(request, error);
-        }
+        },
+        success: function(json){ loadJsonIntoHtml(json);}
     });
     
-}
-                  
+});                  
                 
-function loadData(json){
+function loadJsonIntoHtml(locations){
     
-    console.log(json);
     var el = "";
     
-    for (let i = 0; i < json.locations.length; i++) {
+    for (let i = 0; i < locations.length; i++) {
         
-        let location = json.locations[i];
+        let location = locations[i];
         
         console.log(location.id);
         
         //CREATE A LI ELEMENT WITH ALL ATTRIBUTES NEEDED
-        el += '<div class="col-lg-3 col-md-4 col-sm-6 col-12 mb-4 text-center"><a class="card" href="location.html?id='+location.id+'"><img class="card-img-top" src="'+location.image+'"><h3 class="card-title pt-2 link-custom">'+location.name+'</h3></a></div>';
+        el += '<div class="simple-card-container col-lg-3 col-md-4 col-sm-6 col-12 mb-4 text-center animated fadeIn" style="animation-delay: '+i*0.1+'s;""><a class="card" href="location.html?id='+location.id+'"><img class="card-img-top" src="'+location.image+'"><h5 class="card-title pt-2">'+location.name+'</h5></a></div>';
         
 
     }
