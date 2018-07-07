83d8242f5417efcc4456b583db93a6178536b89f
padrexolo <padrexloco@gmail.com>
2018-07-07 12:01:38 +0200

    minor: refactoring

--- public/assets/js/location.js
+++ public/assets/js/location.js
@@ -1,6 +1,69 @@
-var URL = function () {
-    // This function is anonymous, is executed immediately and 
-    // the return value is assigned to QueryString!
+let URL = getURL();
+
+$(document).ready(function(){
+
+    $.ajax({
+        method: "GET",
+        dataType: "json",
+        url: "/rest/locations/"+URL.id,
+        error: function (request, error) {
+            console.log(request, error);
+        },
+        success: function(json){ 
+            loadJsonIntoHtml(json);
+        }
+    });
+
+    $.ajax({
+        method: "GET",
+        dataType: "json",
+        url: "/rest/locations/" + URL.id + "/photogallery", //SERVER URL
+        error: function (request, error) {
+            console.log(request, error);
+        },
+        success: function(json){
+            loadPhotogallery(json);
+        }
+
+    });
+});
+
+function loadJsonIntoHtml(currentLocation){
+
+    $(".LOCATION-NAME").text(currentLocation.name);
+    $(".LOCATION-DESCRIPTION").html(currentLocation.description);
+    $(".LOCATION-IMAGE").attr("src", currentLocation.image);
+    $(".LOCATION-PRACTICAL-INFO").html(currentLocation.practicalInfo);
+
+    $(".LOCATION-TELEPHONE").attr("href", "tel:"+ currentLocation.telephone);
+    $(".LOCATION-TELEPHONE span").text(currentLocation.telephone);
+
+    $(".LOCATION-EMAIL").attr("href", "mailto:"+ currentLocation.email);
+    $(".LOCATION-EMAIL span").text(currentLocation.email);
+
+    $(".LOCATION-ADDRESS").attr("href", "http://maps.google.com/?daddr="+ currentLocation.address);
+    $(".LOCATION-ADDRESS span").text(currentLocation.address);
+
+    $('.SERVICES-LINK').attr("href","services.html#locationId="+currentLocation.id);
+
+}
+
+function loadPhotogallery(photogallery){
+
+    let el= "";
+    
+    for(let i=0; i<photogallery.length; i++){
+        el += '<div class="col-lg-4 col-md-6 col-12 d-block mb-4 h-100"><a href ="'+photogallery[i].image+'" target="_blank"><img class="img-fluid img-thumbnail" src="'+photogallery[i].image+'" alt=""></a></div>'
+    }
+    
+    $(".LOCATION-PHOTOGALLERY").append(el);   
+
+}
+
+// ---
+//HELPER function to handle the URL query
+// ---
+function getURL() {
     var query_string = {};
     var query = window.location.search.substring(1);
     var vars = query.split("&");
@@ -19,58 +82,4 @@ var URL = function () {
         }
     }
     return query_string;
-}();
-
-$(document).ready(function(){
-
-    $.ajax({
-        method: "GET",
-        dataType: "json",
-        url: "/rest/locations/"+URL.id,
-        error: function (request, error) {
-            console.log(request, error);
-        },
-        success: function(json){ loadJsonIntoHtml(json);}
-    });
-
-});
-
-function loadJsonIntoHtml(currentLocation){
-
-    $(".LOCATION-NAME").text(currentLocation.name);
-    $(".LOCATION-DESCRIPTION").html(currentLocation.description);
-    $(".LOCATION-IMAGE").attr("src", currentLocation.image);
-    $(".LOCATION-PRACTICAL-INFO").html(currentLocation.practicalInfo);
-    
-    $(".LOCATION-TELEPHONE").attr("href", "tel:"+ currentLocation.telephone);
-    $(".LOCATION-TELEPHONE span").text(currentLocation.telephone);
-    
-    $(".LOCATION-EMAIL").attr("href", "mailto:"+ currentLocation.email);
-    $(".LOCATION-EMAIL span").text(currentLocation.email);
-    
-    $(".LOCATION-ADDRESS").attr("href", "http://maps.google.com/?daddr="+ currentLocation.address);
-    $(".LOCATION-ADDRESS span").text(currentLocation.address);
-
-    
-    $('.SERVICES-LINK').attr("href","services.html#locationId="+currentLocation.id);
-    
-    $.ajax({
-        method: "GET",
-        dataType: "json",
-        url: "/rest/locations/" + currentLocation.id + "/photogallery", //SERVER URL
-        error: function (request, error) {
-            console.log(request, error);
-        },
-        success: function(photogallery){
-        
-            let el= "";
-            for(let i=0; i<photogallery.length; i++){
-                el += '<div class="col-lg-4 col-md-6 col-12 d-block mb-4 h-100"><a href ="'+photogallery[i].image+'" target="_blank"><img class="img-fluid img-thumbnail" src="'+photogallery[i].image+'" alt=""></a></div>'
-                }
-            $(".LOCATION-PHOTOGALLERY").append(el);   
-        
-        }
-
-    });   
-
-}
+};
