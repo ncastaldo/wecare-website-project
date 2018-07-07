60e2ee405ab40d9043c78d5821d5c7f3a8f51786
padrexolo <padrexloco@gmail.com>
2018-07-07 12:06:47 +0200

    minor refactoring on person page...

--- public/assets/js/person.js
+++ public/assets/js/person.js
@@ -1,6 +1,61 @@
-var URL = function () {
-    // This function is anonymous, is executed immediately and 
-    // the return value is assigned to QueryString!
+let URL = getURL();
+let personId = URL.id;
+
+$(document).ready(function(){
+
+    $.ajax({
+        method: "GET",
+        dataType: "json",
+        url: "/rest/people/"+personId,
+        error: function (request, error) {
+            console.log(request, error);
+        },
+        success: function(json){ loadJsonPersonIntoHtml(json);}
+    });
+    
+    $.ajax({
+        method: "GET",
+        dataType: "json",
+        url: "/rest/people/"+personId+"/services",
+        error: function (request, error) {
+            console.log(request, error);
+        },
+        success: function(json){ loadJsonServicesIntoHtml(json);}
+    });
+    
+});
+
+function loadJsonPersonIntoHtml(person){
+    
+    $(".PERSON-FIRSTNAME").text(person.firstname);
+    $(".PERSON-LASTNAME").text(person.lastname);
+    $(".PERSON-FULLNAME").text(person.firstname + " " + person.lastname);
+    $(".PERSON-PROFESSION").html(person.profession);
+    $(".PERSON-DESCRIPTION").html(person.description);
+    $(".PERSON-IMAGE").attr("src", person.image);
+
+}
+
+function loadJsonServicesIntoHtml(services){
+    
+    let el = "";
+
+    for (let i = 0; i < services.length; i++) {
+
+        let service = services[i];
+        el += '<div class="little-card-container text-center col-lg-2 col-md-3 col-sm-4 col-6 mb-3 animated fadeIn" style="animation-delay: '+(i+1)*0.1+'s;"><a class="card" href="service.html?id='+service.id+'"><div class="card-img-container"><img class="card-img-top" src="'+service.image+'"></div><div class="card-title-container"><h6 class="card-title link-custom">'+service.name+'</h6></div></a></div>';
+        
+    }
+    
+    $(".PERSON-SERVICES").append(el);
+    
+}
+
+
+// ---
+//HELPER function to handle the URL query
+// ---
+function getURL() {
     var query_string = {};
     var query = window.location.search.substring(1);
     var vars = query.split("&");
@@ -19,73 +74,4 @@ var URL = function () {
         }
     }
     return query_string;
-}();
-
-let personId = URL.id;
-
-$(document).ready(function(){
-
-    performAjaxPersonCall();
-    
-    performAjaxServicesCall();
-    
-});
-
-function performAjaxPersonCall(){
-    
-    $.ajax({
-        method: "GET",
-        dataType: "json",
-        url: "/rest/people/"+personId,
-        error: function (request, error) {
-            console.log(request, error);
-        },
-        success: function(json){ loadJsonPersonIntoHtml(json);}
-    });
-    
-}
-
-function loadJsonPersonIntoHtml(person){
-    
-    $(".PERSON-FIRSTNAME").text(person.firstname);
-    $(".PERSON-LASTNAME").text(person.lastname);
-    $(".PERSON-FULLNAME").text(person.firstname + " " + person.lastname);
-    $(".PERSON-PROFESSION").html(person.profession);
-    $(".PERSON-DESCRIPTION").html(person.description);
-    $(".PERSON-IMAGE").attr("src", person.image);
-
-}
-
-function performAjaxServicesCall(){
-    
-    $.ajax({
-        method: "GET",
-        dataType: "json",
-        url: "/rest/people/"+personId+"/services",
-        error: function (request, error) {
-            console.log(request, error);
-        },
-        success: function(json){ loadJsonServicesIntoHtml(json);}
-    });
-    
-}
-
-function loadJsonServicesIntoHtml(services){
-    
-    let el = "";
-    
-    console.log(services.length)
-
-    for (let i = 0; i < services.length; i++) {
-
-        let service = services[i];
-
-        el += '<div class="little-card-container text-center col-lg-2 col-md-3 col-sm-4 col-6 mb-3 animated fadeIn" style="animation-delay: '+(i+1)*0.1+'s;"><a class="card" href="service.html?id='+service.id+'"><div class="card-img-container"><img class="card-img-top" src="'+service.image+'"></div><div class="card-title-container"><h6 class="card-title link-custom">'+service.name+'</h6></div></a></div>';
-
-        console.log("hci")
-        
-    }
-    
-    $(".PERSON-SERVICES").append(el);
-    
-}
+};
